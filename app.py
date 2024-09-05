2443f829567f60ea8faada7a38baf0125fa1430f
Zizu02 <habibyazar7@gmail.com>
2024-09-05 13:56:06 +0300

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template_string
 import base64
 import hmac
 import hashlib
@@ -8,14 +8,12 @@ import os
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://sapphire-algae-9ajt.squarespace.com"}})  # Sadece belirli bir domain için CORS izni verildi
+CORS(app, resources={r"/*": {"origins": "https://sapphire-algae-9ajt.squarespace.com"}})
 
-# PayTR için gerekli bilgiler
 MERCHANT_ID = '492579'
 MERCHANT_KEY = b'Gxm6ww6x6hbPJmg6'
 MERCHANT_SALT = b'RbuMk9kDZ2bCa5K2'
 
-# PayTR Token oluşturma
 def create_paytr_token(merchant_id, merchant_key, merchant_salt, user_ip, merchant_oid, email, payment_amount, user_basket, no_installment, max_installment, currency, test_mode):
     hash_str = f"{merchant_id}{user_ip}{merchant_oid}{email}{payment_amount}{user_basket.decode()}{no_installment}{max_installment}{currency}{test_mode}"
     return base64.b64encode(hmac.new(merchant_key, hash_str.encode() + merchant_salt, hashlib.sha256).digest())
@@ -29,11 +27,9 @@ def create_payment():
     user_address = data.get('user_address')
     user_phone = data.get('user_phone')
     merchant_oid = data.get('merchant_oid')
-    
-    # Sepet içeriği
+
     user_basket = base64.b64encode(json.dumps([['Ürün Adı', payment_amount, 1]]).encode())
-    
-    # PayTR için gerekli parametreler
+
     params = {
         'merchant_id': MERCHANT_ID,
         'user_ip': request.remote_addr,
@@ -59,20 +55,25 @@ def create_payment():
         'test_mode': '1'
     }
 
-    # PayTR'ye istek gönder
     response = requests.post('https://www.paytr.com/odeme/api/get-token', data=params)
     res = json.loads(response.text)
 
     if res['status'] == 'success':
-        return jsonify({'token': res['token']})
+        # Başarılı token döndü, frontend'de iFrame'i oluşturmak için bu token'ı kullanacağız.
+        token = res['token']
+        iframe_code = f'''
+            <script src="https://www.paytr.com/js/iframeResizer.min.js"></script> 
+            <iframe src="https://www.paytr.com/odeme/guvenli/{token}" id="paytriframe" frameborder="0" scrolling="no" style="width: 100%;"></iframe>
+            <script>iFrameResize({{}},'#paytriframe');</script>
+        '''
+        return render_template_string(iframe_code)
     else:
         return jsonify(res)
 
-# Kök URL için bir rota ekleyelim
 @app.route('/')
 def home():
     return 'Hello, Render! Uygulama çalışıyor.'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Render'den gelen portu al
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
