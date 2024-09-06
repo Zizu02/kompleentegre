5fa80576fea0099cbedf966eb75aaf7912b0eb1d
Zizu02 <habibyazar7@gmail.com>
2024-09-06 14:45:28 +0300

    app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def create_payment():
     else:
         return jsonify(res)
 
-@app.route('/paytr_callback', methods=['POST'])
+@app.route('/paytr_callback', methods=['POST', 'GET'])
 def paytr_callback():
     print(f"Headers: {request.headers}")
     print(f"Body: {request.data}")
